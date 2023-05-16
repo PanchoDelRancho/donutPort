03eb31b7323dc8b1a4b4f41ae62942cc6eb6fbda
PanchoDelRancho <faparicio.fa@gmail.com>
2023-05-16 12:17:37 -0500

    skills added

--- src/aboutComp/skills.jsx
+++ src/aboutComp/skills.jsx
@@ -1,7 +1,28 @@
-import { SiJavascript, SiMongodb } from "react-icons/si";
-import { BsFiletypeHtml, BsFiletypeCss } from "react-icons/bs";
-import { FaNodeJs, FaReact } from "react-icons/fa";
-import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiJavascript,
+  SiMongodb,
+  SiNeovim,
+  SiIos,
+  SiChakraui,
+  SiMui,
+  SiAdobephotoshop,
+} from "react-icons/si";
+import { BsFiletypeHtml, BsFiletypeCss, BsFiletypeScss } from "react-icons/bs";
+import {
+  FaNodeJs,
+  FaReact,
+  FaGit,
+  FaNpm,
+  FaBootstrap,
+  FaLinux,
+} from "react-icons/fa";
+import {
+  TbBrandNextjs,
+  TbApi,
+  TbBrandVscode,
+  TbBrandReactNative,
+} from "react-icons/tb";
+import { AiFillAndroid } from "react-icons/ai";
 
 export default function Skills() {
   return (
@@ -9,11 +30,23 @@ export default function Skills() {
       <div className="skillTitle">Skills</div>
       <div className="skillIconContainer">
         <BsFiletypeHtml className="skillIcon" />
-        <FaNodeJs className="skillIcon" />
         <BsFiletypeCss className="skillIcon" />
+        <BsFiletypeScss className="skillIcon" />{" "}
+        <FaNodeJs className="skillIcon" />
         <SiMongodb className="skillIcon" />
+        <TbApi className="skillIcon" />
+        <FaNpm className="skillIcon" />
+        <FaGit className="skillIcon" />
         <FaReact className="skillIcon" />
         <TbBrandNextjs className="skillIcon" />
+        <TbBrandReactNative className="skillIcon" />
+        <SiIos className="skillIcon" />
+        <AiFillAndroid className="skillIcon" />
+        <FaBootstrap className="skillIcon" />
+        <SiChakraui className="skillIcon" />
+        <SiMui className="skillIcon" />
+        <SiAdobephotoshop className="skillIcon" />
+        <FaLinux className="skillIcon" />
       </div>
     </div>
   );
